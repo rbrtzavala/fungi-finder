bf5f4b0f024feee7f498aaaa49173ecf96814801
Robert Zavala <rbrtzavala@gmail.com>
2025-07-22 15:14:58 -0600

    setup js filtering for cards on guide page

---
+++ mushroom-filter.js
@@ -0,0 +1,38 @@
+const cards = document.querySelectorAll(".mushroom-guide .card");
+
+const seasonalFilter = document.querySelector("#season");
+const edibleFilter = document.querySelector("#edible");
+
+const currentFilters = {
+  season: "all",
+  edible: "all",
+};
+
+seasonalFilter.addEventListener("change", updateFilter);
+edibleFilter.addEventListener("change", updateFilter);
+
+function updateFilter(e) {
+  const filterType = e.target.name;
+  currentFilters[filterType] = e.target.value;
+
+  filterCards();
+}
+
+function filterCards() {
+  cards.forEach((card) => {
+    const season = card.querySelector("[data-season]").dataset.season;
+    const edible = card.querySelector("[data-edible]").dataset.edible;
+
+    const matchesSeason = currentFilters.season === season;
+    const matchesEdible = currentFilters.edible === edible;
+
+    if (
+      (matchesEdible || currentFilters.edible === "all") &&
+      (matchesSeason || currentFilters.season === "all")
+    ) {
+      card.hidden = false;
+    } else {
+      card.hidden = true;
+    }
+  });
+}
